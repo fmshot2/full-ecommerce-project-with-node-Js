61f698d8102938dafce3a081a637e0ef5e4e7896
fmshot2 <femishotolaa@gmail.com>
2022-03-05 06:43:45 +0100

    fixed compass crash, created product model

--- backend/app.js
+++ backend/app.js
@@ -11,17 +11,4 @@ const products = require('./routes/product');
 
 app.use('/api/v1', products)
 
-const express = require('express');
-const  app = express();
-
-app.use(express.json());
-
-
-// Import all routes
-const products = require('./routes/product');
-
-
-
-app.use('/api/v1', products)
-
 module.exports = app
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,11 @@
+const Product = require('../models/product')
+
+exports.newProducts = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: 'This route will show all products in the database.'
+    })
+}
 exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success: true,
